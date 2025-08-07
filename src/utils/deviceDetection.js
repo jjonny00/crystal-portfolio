6f5706bbedba3a12d0f37d046b1c09acf9a0ce18
J Jonny <78575300+jjonny00@users.noreply.github.com>
2025-08-07 12:43:36 -0400

    chore: remove unused device detection helpers (#107)

--- src/utils/deviceDetection.js
+++
@@ -1,9 +0,0 @@
-// src/utils/deviceDetection.js
-// Simplified helpers for basic device checks
-
-export const isMobile = () => /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
-
-export const isTablet = () => /(iPad|Android(?!.*Mobile))/i.test(navigator.userAgent);
-
-export const isDesktop = () => !isMobile() && !isTablet();
-
