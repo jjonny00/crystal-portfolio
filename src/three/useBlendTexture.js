72c491429721ce017131dea782a9e0c323441973
J Jonny <78575300+jjonny00@users.noreply.github.com>
2025-09-08 23:19:49 -0400

    fix: insert blend uniform robustly

--- src/three/useBlendTexture.js
+++ src/three/useBlendTexture.js
@@ -16,31 +16,30 @@ export function useBlendTexture(material, { initialBlend = 0 } = {}) {
       material.userData._blendUniform = shader.uniforms.uBlend;
       material.userData._currentBlend = current;
 
-      if (!shader.fragmentShader.includes("uBlend")) {
-        shader.fragmentShader = shader.fragmentShader.replace(
-          "void main() {",
-          "uniform float uBlend;\nvoid main() {"
+      shader.fragmentShader = shader.fragmentShader
+        .replace(
+          '#include <clipping_planes_pars_fragment>',
+          '#include <clipping_planes_pars_fragment>\nuniform float uBlend;'
+        )
+        .replace(
+          '#include <map_fragment>',
+          [
+            '#ifdef USE_MAP',
+            '  vec4 sampledDiffuseColor = texture2D( map, vMapUv );',
+            '#ifdef DECODE_VIDEO_TEXTURE',
+            '    sampledDiffuseColor = vec4( mix( vec3(0.0), sampledDiffuseColor.rgb, saturate( sampledDiffuseColor.a ) ), 1.0 );',
+            '#endif',
+            '  sampledDiffuseColor = mapTexelToLinear( sampledDiffuseColor );',
+            '',
+            '  vec3 baseCol = diffuseColor.rgb;',
+            '  vec3 mappedCol = baseCol * sampledDiffuseColor.rgb;',
+            '',
+            '  // 0 = base only, 1 = base*map',
+            '  diffuseColor.rgb = mix( baseCol, mappedCol, uBlend );',
+            '  diffuseColor.a *= sampledDiffuseColor.a;',
+            '#endif'
+          ].join('\n')
         );
-      }
-
-      shader.fragmentShader = shader.fragmentShader.replace(
-        "#include <map_fragment>",
-        [
-          "#ifdef USE_MAP",
-          "vec4 sampledDiffuseColor = texture2D( map, vMapUv );",
-          "#ifdef DECODE_VIDEO_TEXTURE",
-          "  sampledDiffuseColor = vec4( mix( vec3(0.0), sampledDiffuseColor.rgb, saturate( sampledDiffuseColor.a ) ), 1.0 );",
-          "#endif",
-          "sampledDiffuseColor = mapTexelToLinear( sampledDiffuseColor );",
-          "",
-          "vec3 baseCol = diffuseColor.rgb;",
-          "vec3 mappedCol = baseCol * sampledDiffuseColor.rgb;",
-          "",
-          "// 0 = base only, 1 = base*map",
-          "diffuseColor.rgb = mix( baseCol, mappedCol, uBlend );",
-          "#endif"
-        ].join("\n")
-      );
     };
 
     material.needsUpdate = true;
