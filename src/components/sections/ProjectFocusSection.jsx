955e78795b9ee5ee3148475866e9274941783732
J Jonny <78575300+jjonny00@users.noreply.github.com>
2026-03-14 15:45:41 -0400

    Align project content within left-half column

--- src/components/sections/ProjectFocusSection.jsx
+++ src/components/sections/ProjectFocusSection.jsx
@@ -7,7 +7,6 @@ const ProjectFocusSection = ({ project, isMobile = false }) => {
   const headlineColor = project.headlineColor || project.color || '#ffffff';
   const sectionIdKey = project.crystalKey || project.facetKey;
 
-  const horizontalPadding = isMobile ? '22px' : 'clamp(56px, 7vw, 120px)';
   const contentWidth = isMobile ? '100%' : 'min(34vw, 640px)';
 
   return (
@@ -24,102 +23,71 @@ const ProjectFocusSection = ({ project, isMobile = false }) => {
         alignItems: 'center',
         justifyContent: 'flex-start',
         background: 'transparent',
-        paddingLeft: horizontalPadding,
-        paddingRight: horizontalPadding,
         boxSizing: 'border-box'
       }}
     >
       <div
-        style={{
-          width: contentWidth,
-          maxWidth: '100%'
-        }}
+        style={
+          isMobile
+            ? {
+                width: '100%',
+                paddingLeft: '22px',
+                paddingRight: '22px',
+                boxSizing: 'border-box'
+              }
+            : {
+                width: '50%',
+                height: '100%',
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'center',
+                paddingLeft: 'clamp(24px, 3vw, 64px)',
+                paddingRight: 'clamp(24px, 3vw, 64px)',
+                boxSizing: 'border-box'
+              }
+        }
       >
-        <Headline
-          as="h1"
+        <div
           style={{
-            margin: 0,
-            color: headlineColor,
-            fontFamily: '"ivypresto-display", "IvyPresto Display", "Playfair Display", Georgia, serif',
-            fontSize: isMobile ? '44px' : '64px',
-            fontStyle: 'normal',
-            fontWeight: 400,
-            lineHeight: 'normal',
-            textTransform: 'uppercase',
-            '--headline-ink': headlineColor,
-            '--headline-glow1': headlineColor,
-            '--headline-glow2': headlineColor
+            width: contentWidth,
+            maxWidth: '100%'
           }}
         >
-          {project.title}
-        </Headline>
-
-        <p
-          style={{
-            margin: '8px 0 18px',
-            color: 'rgb(from #E2DCC3 r g b / 0.6)',
-            fontFamily: '"acumin-variable", "Acumin VF", sans-serif',
-            fontSize: '16px',
-            fontStyle: 'normal',
-            fontWeight: 400,
-            lineHeight: '30px',
-            letterSpacing: '-0.32px',
-            textTransform: 'uppercase'
-          }}
-        >
-          {project.subtitle}
-        </p>
-
-        <p
-          style={{
-            margin: 0,
-            color: 'rgb(from #E2DCC3 r g b / 0.85)',
-            fontFamily: '"acumin-variable", "Acumin VF", sans-serif',
-            fontSize: isMobile ? '22px' : '24px',
-            fontStyle: 'normal',
-            fontWeight: 300,
-            lineHeight: '30px',
-            letterSpacing: '-0.48px'
-          }}
-        >
-          {project.description}
-        </p>
-
-        {project.secondaryCopy && (
-          <p
+          <Headline
+            as="h1"
             style={{
-              margin: '30px 0 0',
-              color: 'rgb(from #E2DCC3 r g b / 0.85)',
-              fontFamily: '"acumin-variable", "Acumin VF", sans-serif',
-              fontSize: isMobile ? '22px' : '24px',
+              margin: 0,
+              color: headlineColor,
+              fontFamily: '"ivypresto-display", "IvyPresto Display", "Playfair Display", Georgia, serif',
+              fontSize: isMobile ? '44px' : '64px',
               fontStyle: 'normal',
-              fontWeight: 300,
-              lineHeight: '30px',
-              letterSpacing: '-0.48px'
+              fontWeight: 400,
+              lineHeight: 'normal',
+              textTransform: 'uppercase',
+              '--headline-ink': headlineColor,
+              '--headline-glow1': headlineColor,
+              '--headline-glow2': headlineColor
             }}
           >
-            {project.secondaryCopy}
-          </p>
-        )}
+            {project.title}
+          </Headline>
 
-        {project.metrics && (
           <p
             style={{
-              margin: '30px 0 0',
-              color: 'rgb(from #E2DCC3 r g b / 0.85)',
+              margin: '8px 0 18px',
+              color: 'rgb(from #E2DCC3 r g b / 0.6)',
               fontFamily: '"acumin-variable", "Acumin VF", sans-serif',
-              fontSize: isMobile ? '22px' : '24px',
+              fontSize: '16px',
               fontStyle: 'normal',
-              fontWeight: 300,
+              fontWeight: 400,
               lineHeight: '30px',
-              letterSpacing: '-0.48px'
+              letterSpacing: '-0.32px',
+              textTransform: 'uppercase'
             }}
           >
-            {project.metrics}
+            {project.subtitle}
           </p>
-        )}
 
-        {project.roles && (
           <p
             style={{
               margin: 0,
@@ -132,27 +100,78 @@ const ProjectFocusSection = ({ project, isMobile = false }) => {
               letterSpacing: '-0.48px'
             }}
           >
-            {project.roles}
+            {project.description}
           </p>
-        )}
 
-        {project.cta && (
-          <p
-            style={{
-              margin: '46px 0 0',
-              color: headlineColor,
-              textAlign: 'center',
-              fontFamily: '"acumin-variable", "Acumin VF", sans-serif',
-              fontSize: '24px',
-              fontStyle: 'normal',
-              fontWeight: 600,
-              lineHeight: '30px',
-              letterSpacing: '-0.48px'
-            }}
-          >
-            {project.cta}
-          </p>
-        )}
+          {project.secondaryCopy && (
+            <p
+              style={{
+                margin: '30px 0 0',
+                color: 'rgb(from #E2DCC3 r g b / 0.85)',
+                fontFamily: '"acumin-variable", "Acumin VF", sans-serif',
+                fontSize: isMobile ? '22px' : '24px',
+                fontStyle: 'normal',
+                fontWeight: 300,
+                lineHeight: '30px',
+                letterSpacing: '-0.48px'
+              }}
+            >
+              {project.secondaryCopy}
+            </p>
+          )}
+
+          {project.metrics && (
+            <p
+              style={{
+                margin: '30px 0 0',
+                color: 'rgb(from #E2DCC3 r g b / 0.85)',
+                fontFamily: '"acumin-variable", "Acumin VF", sans-serif',
+                fontSize: isMobile ? '22px' : '24px',
+                fontStyle: 'normal',
+                fontWeight: 300,
+                lineHeight: '30px',
+                letterSpacing: '-0.48px'
+              }}
+            >
+              {project.metrics}
+            </p>
+          )}
+
+          {project.roles && (
+            <p
+              style={{
+                margin: 0,
+                color: 'rgb(from #E2DCC3 r g b / 0.85)',
+                fontFamily: '"acumin-variable", "Acumin VF", sans-serif',
+                fontSize: isMobile ? '22px' : '24px',
+                fontStyle: 'normal',
+                fontWeight: 300,
+                lineHeight: '30px',
+                letterSpacing: '-0.48px'
+              }}
+            >
+              {project.roles}
+            </p>
+          )}
+
+          {project.cta && (
+            <p
+              style={{
+                margin: '46px 0 0',
+                color: headlineColor,
+                textAlign: 'center',
+                fontFamily: '"acumin-variable", "Acumin VF", sans-serif',
+                fontSize: '24px',
+                fontStyle: 'normal',
+                fontWeight: 600,
+                lineHeight: '30px',
+                letterSpacing: '-0.48px'
+              }}
+            >
+              {project.cta}
+            </p>
+          )}
+        </div>
       </div>
     </section>
   );
